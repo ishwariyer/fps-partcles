55ea084eb547ca19199bfe99321be8548eeebaa6
ishwariyer <ishwar.siyer@gmail.com>
2020-09-01 00:48:32 +0530

    Level Design

---
+++ Level Design/Level Design/Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour
+{
+    public AudioClip clip;
+    bool CanFlicker = true;
+    private void Update()
+    {
+        StartCoroutine(Flicker ());
+    }
+
+    IEnumerator Flicker()
+    {
+        if(CanFlicker)
+        {
+            CanFlicker = false;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f , 0.5f));
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 5));
+            CanFlicker = true;
+        }
+    }
+}
---
+++ Level Design/Packages/unity level design files/level design project/Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderClap : MonoBehaviour
+{
+	public AudioClip clip;
+	bool canFlicker = true;
+
+	void Update ()
+	{
+		StartCoroutine (Flicker ());
+	}
+
+	IEnumerator Flicker ()
+	{
+		if ( canFlicker )
+		{
+			canFlicker = false;
+			audio.PlayOneShot ( clip );
+			light.enabled = true;
+			yield return new WaitForSeconds ( Random.Range ( 0.1f, 0.4f ) );
+			light.enabled = false;
+			yield return new WaitForSeconds ( Random.Range ( 0.1f, 5 ) );
+			canFlicker = true;
+		}
+	}
+}
